d96414e97193805148526a2a4189c7a2e6cee491
Jack Polgar <jack@polgar.id.au>
2015-12-15 20:19:15 +1100

    Exception on no route found.

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -98,7 +98,7 @@ class AppKernel
         if ($route) {
             Kernel::process("{$this->path}/{$route}");
         } else {
-            die(sprintf("No route matches [%s]", Request::$pathInfo));
+            throw new Exception(sprintf("No route matches [%s]", Request::$pathInfo));
         }
     }
 }
